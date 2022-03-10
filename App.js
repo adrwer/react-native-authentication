279b80f213f2945e5c70d7a258a62e3bb8c73de4
Adrian Werimo <adrwer7@gmail.com>
2022-03-11 02:55:51 +0300

    Folder structure

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ import {
   StyleSheet,
   Text
 } from 'react-native';
-
+import SignInScreen from './src/screens/SignInScreen';
 
 const App = () => {
   
   return (
     <SafeAreaView style={styles.root}>
-      <Text>Hello World</Text>
+      <SignInScreen />
     </SafeAreaView>
   );
 };
