1f49e42ade6a0174370c09052deddcebbe28c6bd
Adrian Werimo <adrwer7@gmail.com>
2022-03-11 17:42:46 +0300

    Custom button component

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native'
+import { View, Text, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native'
 import React, {useState} from 'react'
 import Logo from '../../../assets/images/Logo_1.png'
 import CustomInput from '../../components/CustomInput'
+import CustomButton from '../../components/CustomButton'
 
 const SignInScreen = () => {
   const [username, setUsername] = useState('')
@@ -9,13 +10,43 @@ const SignInScreen = () => {
 
   const {height} = useWindowDimensions()
 
+  const onSignInPressed = () => {
+    console.warn('Signed In')
+  }
+
+  const onSignInFacebookPressed = () => {
+    console.warn('Signed In Facebook')
+  }
+
+  const onSignInGooglePressed = () => {
+    console.warn('Signed In Google')
+  }
+
+  const onForgotPasswordPressed = () => {
+    console.warn("Forgot Password")
+  }
+
+  const onSignUpPressed = () => {
+    console.warn('Sign Up')
+  }
+
   return (
-    <View style={styles.root}>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
       <Image source={Logo} style={[styles.logo, {height: height*0.3}]} resizeMode='contain' />
 
       <CustomInput placeholder='Username' value={username} setValue={setUsername} />
       <CustomInput placeholder='Password' value={password} setValue={setPassword} secureTextEntry/>
+
+      <CustomButton text="Sign In" onPress={onSignInPressed} />
+      <CustomButton text="Forgot Password?" onPress={onForgotPasswordPressed} type='TERTIARY' />
+
+      <CustomButton text="Sign In with Facebook" onPress={onSignInFacebookPressed} bgColor="#E7EAF4" fgColor="#4765A9" />
+      <CustomButton text="Sign In with Google" onPress={onSignInGooglePressed} bgColor="#FAE9EA" fgColor="#DD4D44" />
+
+      <CustomButton text="Don't have an account? Create one" onPress={onSignUpPressed} type='TERTIARY' />
     </View>
+    </ScrollView>
   )
 }
 
